84b4e5a8a54c6ca97f05c6ae020ed3bbaf1a0106
Xavi Ablaza <xlablaza@gmail.com>
2019-04-11 09:39:33 -0700

    Migrate database

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_09_223624) do
+ActiveRecord::Schema.define(version: 2019_04_11_163135) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "branches", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "delayed_jobs", force: :cascade do |t|
     t.integer "priority", default: 0, null: false
@@ -28,6 +34,18 @@ ActiveRecord::Schema.define(version: 2019_04_09_223624) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.index ["priority", "run_at"], name: "delayed_jobs_priority"
+  end
+
+  create_table "regional_managers", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_regional_managers_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_regional_managers_on_reset_password_token", unique: true
   end
 
   create_table "users", force: :cascade do |t|
