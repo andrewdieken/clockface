2d4ed9020fae67fb23242924441083f18d75d27b
Xavi Ablaza <xlablaza@gmail.com>
2019-04-18 22:42:00 -0700

    Add capybara_helpers.rb

---
+++ spec/support/capybara_helpers.rb
@@ -0,0 +1,11 @@
+module CapybaraHelpers
+  def sign_in_as(user)
+    visit new_user_session_path
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+    click_button "Log in"
+  end
+end
+
+RSpec.configuration.include CapybaraHelpers, type: :feature
+RSpec.configuration.include CapybaraHelpers, type: :system
