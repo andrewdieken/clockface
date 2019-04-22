f5e660c94b422f32f3f6493db027dd3b8e9c4d2a
Xavi Ablaza <xlablaza@gmail.com>
2019-04-21 22:10:03 -0700

    Pass branch id and name instead of object
    Branch id is the PersonGroup id

--- spec/concepts/person_group/operations/create_spec.rb
+++ spec/concepts/person_group/operations/create_spec.rb
@@ -3,10 +3,13 @@ require "rails_helper"
 module PersonGroup
   module Operations
     RSpec.describe Create, vcr: { record: :once, match_requests_on: [:body] } do
-      let(:branch) { create(:branch) }
 
       it "creates a person group" do
-        op = described_class.(branch: branch)
+        op = described_class.(
+          branch: {
+            id: 1,
+            name: "branch_1"
+          })
 
         expect(op.success?).to be_truthy
       end
