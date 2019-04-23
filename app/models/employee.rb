fb7e1b6917b43c1a1bc1ad5092fe27f6b907d674
Xavi Ablaza <xlablaza@gmail.com>
2019-04-22 19:17:49 -0700

    Add full_name to Employee

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,4 +1,8 @@
 class Employee < ApplicationRecord
   belongs_to :branch
   has_many :shifts
+
+  def full_name
+    [first_name, last_name].join(" ")
+  end
 end
--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -3,4 +3,13 @@ require 'rails_helper'
 RSpec.describe Employee, type: :model do
   it { should belong_to :branch }
   it { should have_many :shifts }
+  let (:branch) { create(:branch) }
+
+  describe "#full_name" do
+    it 'appends last_name to first_name' do
+      employee = create(:employee, first_name: 'John', last_name: 'Wick', branch: branch)
+
+      expect(employee.full_name).to eq 'John Wick'
+    end
+  end
 end
